4d8c406716bc4484bd11543e43c82a6cb160dedb
ankityadav-projects <ankitprojects0@gmail.com>
2026-01-20 22:35:06 +0530

    Version 2

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
+import dotenv from "dotenv"
+dotenv.config()
 import mongoose from "mongoose";
 
 
 // define mongo DB Url Connection
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
-
+const mongoURL = process.env.DB_URL
+//const mongoURL ="mongodb+srv://yajit289_db_user:Ajit289%40@cluster0.02xgut8.mongodb.net/hotels?retryWrites=true&w=majority";
 mongoose.connect(mongoURL)
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-
+import dotenv from "dotenv"
+dotenv.config()
 import express from 'express'
 import bodyParser from 'body-parser';
 import "./db.js"
 import personRoutes from './routes/personRoutres.js'
 import menuRoutes from './routes/menuRoutes.js'
-
 const app = express();
+const port = process.env.PORT
 app.use(bodyParser.json())
 
 app.use('/person',personRoutes)
@@ -17,6 +18,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running on http://localhost:3000')
 })
